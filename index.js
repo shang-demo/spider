4af9ce310b164d9c6399604a4e24f6129f05cb1a
xinshangshangxin <xinshangshangxin@gmail.com>
2018-06-12 20:35:26 +0800

    refactor: adapt new Errors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
-  Unknown: { code: 1, message: 'unknown error, need feedback' },
-  ParseFailed: { code: 1001, message: 'parse error' },
-  WrongParams: { code: 1002, message: 'params error' },
+  Unknown: 'unknown error, need feedback',
+  ParamsRequired: 'params required',
+  ParseFailed: 'parse error',
+  WrongParams: 'params error',
 };
