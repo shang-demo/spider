c418fbd42f4d763cf3d3bc057f2d3c562594a520
xinshangshangxin <xinshangshangxin@gmail.com>
2018-06-12 15:00:53 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  Unknown: { code: 1, message: 'unknown error, need feedback' },
+  ParseFailed: { code: 1001, message: 'parse error' },
+  WrongParams: { code: 1002, message: 'params error' },
+};
